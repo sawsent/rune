cefddbc8dbb104177ce60b80faa0ca1705194f76
Vicente Figueiredo <vicenteppfigueiredo@gmail.com>
2025-12-15 11:36:29 +0000

    Update context

--- src/rune/context.py
+++ src/rune/context.py
@@ -9,11 +9,17 @@ from rune.storage.settings.settingsstorage import SettingsStorageManager
 class Context:
     _context: Self | None = None
 
-    def __init__(self) -> None:
-        self.settings_manager: SettingsStorageManager = SettingsStorageManager()
+    def __init__(
+            self,
+        settings_manager: SettingsStorageManager | None,
+        encrypter: Encrypter | None,
+        storage_manager: StorageManager | None
+
+    ) -> None:
+        self.settings_manager: SettingsStorageManager = settings_manager or SettingsStorageManager()
         self.settings: Settings = self.settings_manager.load_settings()
-        self.configured_encrypter: Encrypter = get_encrypter(self.settings)
-        self.storage_manager: StorageManager = get_storage_manager(self.settings)
+        self.configured_encrypter: Encrypter = encrypter or get_encrypter(self.settings)
+        self.storage_manager: StorageManager = storage_manager or get_storage_manager(self.settings)
 
     @classmethod
     def get(cls) -> Self:
@@ -22,16 +28,29 @@ class Context:
         raise RuntimeError("Context is not set. Call `Context.build()` first.")
 
     @classmethod
-    def build(cls) -> Self:
+    def build(
+        cls,
+        settings_manager: SettingsStorageManager | None = None,
+        encrypter: Encrypter | None = None,
+        storage_manager: StorageManager | None = None,
+    ) -> Self:
         if not cls._context:
-            cls._context = cls()
+            cls._context = cls(settings_manager, encrypter, storage_manager)
             return cls._context
         raise RuntimeError("Context is already built")
 
     @classmethod
-    def update(cls) -> Self:
-        cls._context = cls()
-        return cls._context
+    def update(
+        cls,
+        settings_manager: SettingsStorageManager | None = None,
+        encrypter: Encrypter | None = None,
+        storage_manager: StorageManager | None = None,
+    ) -> Self:
+        if cls._context:
+            cls._context = cls(settings_manager, encrypter, storage_manager)
+            return cls._context
+        raise RuntimeError("Context is not set. Call `Context.build()` first.")
+
 
     @classmethod
     def reset(cls) -> None:
